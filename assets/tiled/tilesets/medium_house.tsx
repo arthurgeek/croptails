59902cdd90a1515e89fbcf6f0ab70004bbc824eb
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2026-02-02 21:38:48 -0300

    feat(tiled): add medium and small house prefabs
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

---
+++ assets/tiled/tilesets/medium_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="medium_house" tilewidth="144" tileheight="112" tilecount="1" columns="1">
+ <image source="../maps/houses/medium_house.png" width="144" height="112"/>
+</tileset>
